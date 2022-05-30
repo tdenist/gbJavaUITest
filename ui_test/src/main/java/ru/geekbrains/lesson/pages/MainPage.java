8910a4b2248dd50581ff96e1dfd28651c1995ce3
Denis <dent.90@mail.ru>
2022-05-30 11:46:03 +0300

    Увеличено ожидание кнопки Каталог

--- ui_test/src/main/java/ru/geekbrains/lesson/pages/MainPage.java
+++ ui_test/src/main/java/ru/geekbrains/lesson/pages/MainPage.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.lesson.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.page;
@@ -21,7 +24,7 @@ public class MainPage {
 
     @Step("Нажать кнопку \"Каталог\"")
     public CatalogPage clickCatalogButton(){
-        catalogButton.click();
+        catalogButton.should(Condition.visible, Duration.ofSeconds(5)).click();
         return page(CatalogPage.class);
     }
 }
